317eaa33dbd75aabe437592d5099929583138420
mary ann <114210058+maryannintech@users.noreply.github.com>
2025-06-20 17:59:03 +0800

    Add feedback category for favorite recipes; implement feedback message for empty favorites and update rendering logic

--- scripts/recipe-list.js
+++ scripts/recipe-list.js
@@ -2,7 +2,7 @@ import {
   fetchRecipeDetails,
   apiRecipesDetails,
   fetchRecipes,
-  allRecipes
+  allRecipes,
 } from "../data/recipes.js";
 import { renderRecipes } from "./recipe.js";
 import { renderRecipesByCategory } from "./filter.js";
@@ -21,13 +21,14 @@ const recipeListContainer = document.querySelector(".recipes-container");
 const recipeDetailListContainer = document.querySelector(
   ".js-recipe-details-container"
 );
+const feedbackCategory = document.querySelector(".js-feedback-category");
 
 document.querySelectorAll(".filter-btn").forEach((filterBtn) => {
   filterBtn.addEventListener("click", () => {
     const filterId = filterBtn.dataset.filterId;
     renderRecipesByCategory(filterId);
-  })
-})
+  });
+});
 
 export function renderRecipeList() {
   apiRecipesDetails.forEach((recipe) => {
@@ -47,22 +48,35 @@ export function recipeCardClick() {
 }
 
 export function renderFavoriteRecipes() {
+  let favoriteRecipes = [];
+  feedbackCategory.innerHTML = "";
   if (recipeListContainer) {
     recipeListContainer.innerHTML = "";
-    allRecipes.forEach((recipe) => {
-      if (recipe.favorite) {
-        const favoriteRecipeCardHTML = renderRecipes(recipe);
-        recipeListContainer.innerHTML += favoriteRecipeCardHTML;
-      }
+    favoriteRecipes = allRecipes.filter((recipe) => {
+      return recipe.favorite;
+    });
+    favoriteRecipes.forEach((recipe) => {
+      const favoriteRecipeCardHTML = renderRecipes(recipe);
+      recipeListContainer.innerHTML += favoriteRecipeCardHTML;
     });
   } else if (recipeDetailListContainer) {
     recipeDetailListContainer.innerHTML = "";
-    allRecipes.forEach((recipe) => {
-      if (recipe.favorite) {
-        const favoriteRecipeCardHTML = renderRecipes(recipe);
-        recipeDetailListContainer.innerHTML += favoriteRecipeCardHTML;
-      }
+    favoriteRecipes = allRecipes.filter((recipe) => {
+      return recipe.favorite;
     });
+    favoriteRecipes.forEach((recipe) => {
+      const favoriteRecipeCardHTML = renderRecipes(recipe);
+      recipeDetailListContainer.innerHTML += favoriteRecipeCardHTML;
+    });
+  }
+
+  if (favoriteRecipes.length === 0) {
+    feedbackCategory.innerHTML = `
+      <p class="feedback-message">
+        <span class="feedback-category-emphasis">Nothing here yet!</span>
+        No favorite recipes found
+      </p>
+    `;
   }
   recipeCardClick();
 }
