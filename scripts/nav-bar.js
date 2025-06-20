ddb99c72a3ef784ef9c5e5a580ced57b081337b8
mary ann <114210058+maryannintech@users.noreply.github.com>
2025-06-20 11:10:17 +0800

    Enhance navigation bar functionality; remove console log, adjust font size, and add close button styling

--- scripts/nav-bar.js
+++ scripts/nav-bar.js
@@ -1,4 +1,13 @@
 document.querySelector(".bxs-home-alt").addEventListener("click", () => {
-  console.log("Home icon clicked");
   window.location.href = "index.html";
+});
+
+const openNavBarElement = document.querySelector(".js-open-nav-bar");
+
+document.querySelector(".bxs-menu-wide").addEventListener("click", () => {
+  openNavBarElement.classList.remove("hide");
+});
+
+document.querySelector(".js-close-nav-bar").addEventListener("click", () => {
+  openNavBarElement.classList.add("hide");
 });
