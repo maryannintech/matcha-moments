5f206896d0fb5e1be4e6ee7b993eea2261856642
mary ann <114210058+maryannintech@users.noreply.github.com>
2025-06-24 11:31:33 +0800

    Refactor recipe detail handling: increase recipe name maxlength to 50, update category options, and enhance recipe detail rendering logic

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { renderRecipeList } from "./scripts/recipe-list.js";
+import { userRecipes, apiRecipesDetails } from "./data/recipes.js";
 import "./scripts/nav-bar.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   renderRecipeList(); 
+  console.log(userRecipes)
 });
--- scripts/detail-recipe.js
+++ scripts/detail-recipe.js
@@ -2,13 +2,17 @@ import {
   apiRecipesDetails,
   fetchRecipeDetails,
   updateFavoriteStatus,
-
+  userRecipes,
 } from "../data/recipes.js";
-import { renderRecipeDetailCard, renderUserRecipeDetailCard } from "./recipe.js";
+import {
+  renderRecipeDetailCard,
+  renderRecipes,
+  renderUserRecipeCard,
+  renderUserRecipeDetailCard,
+} from "./recipe.js";
 
 const params = new URLSearchParams(window.location.search);
 const recipeId = Number(params.get("id"));
-
 
 async function loadRecipeDetail() {
   if (apiRecipesDetails.length === 0) {
@@ -16,8 +20,16 @@ async function loadRecipeDetail() {
   }
 
   const recipe = apiRecipesDetails.find((r) => r.id === recipeId);
-  console.log("Recipe found:", recipe);
-  recipe ? renderRecipeDetail(recipe, apiRecipesDetails) : "Recipe not found";
+  const userRecipe = userRecipes.find((r) => r.id === recipeId);
+  if (recipe) {
+    renderRecipeDetail(recipe, "apiRecipesDetails");
+  } else if (userRecipe) {
+    renderRecipeDetail(userRecipe, "userRecipesDetails");
+  } else {
+    console.error(`Recipe with ID ${recipeId} not found.`);
+    document.querySelector(".js-recipe-details-container").innerHTML =
+      "<p>Recipe not found.</p>";
+  }
 }
 
 loadRecipeDetail();
@@ -26,7 +38,10 @@ export function renderRecipeDetail(recipe, recipes) {
   const recipeDetailContainer = document.querySelector(
     ".js-recipe-details-container"
   );
-  recipeDetailContainer.innerHTML = renderRecipeDetailCard(recipe);
+  recipeDetailContainer.innerHTML =
+    recipes === "apiRecipesDetails"
+      ? renderRecipeDetailCard(recipe)
+      : renderUserRecipeDetailCard(recipe);
   document.title = `${recipe.title} | matcha moments`;
 
   document.querySelectorAll(".js-favorite-icon").forEach((icon) => {
@@ -38,12 +53,12 @@ export function renderRecipeDetail(recipe, recipes) {
         icon.classList.toggle("bxs-star");
         icon.classList.toggle("bx-star");
         updateFavoriteStatus(recipeId, recipe.favorite);
-        renderRecipeDetailCard(recipe);
+        recipes === "apiRecipesDetails"
+          ? renderRecipes(recipe)
+          : renderUserRecipeCard(recipe);
       } else {
         console.error(`Recipe with ID ${recipeId} not found.`);
       }
-    })
-  })
+    });
+  });
 }
-
-console.log("Recipe details rendered:", apiRecipesDetails);
